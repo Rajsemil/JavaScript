ff8bafa83eecdd3e4fa61c0347088f51cd987c68
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2022-12-21 00:38:21 -0800

    Add files via upload

---
+++ trim.js
@@ -0,0 +1,11 @@
+//console.log("semil");
+let name = "Rajkishor Semil"
+console.log("Name: ",name);
+console.log("Upper Case: ",name.toUpperCase()); //IT IS CHANGED INTO UPPER VALUE
+console.log("Lower Case: ",name.toLowerCase()); // IT IS CHANGED INTO LOWER VALUE
+let newString = name.slice(0,4);
+console.log("Slice Range: ",newString); // IT IS SHOW RANGR 0 STARTING POINT AND 4 ENDING POINT
+console.log(name.length);
+name = name.trim(); // IT IS USED TO REMOVE SPACE 
+console.log("Spqce Remover: ",name);
+console.log(name.length);
